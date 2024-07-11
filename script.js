c97d159e5c2ee1313f01634f4c41014d77e3a44d
marywebby <marywebby1999@gmail.com>
2024-07-11 14:27:54 -0500

    added bootstrap navbar'

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '.')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 });
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
